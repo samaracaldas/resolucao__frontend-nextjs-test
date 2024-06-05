4a52d617ef6bd482cfaf39b5fc6c78da82a4824e
Samara Caldas <samaracaldass@hotmail.com>
2024-06-05 14:51:41 -0300

    6 - Página estática
    - Transformar essa página em uma página estática
    - A página deve ser gerada no momento da build
    - A página deve ser atualizada a cada 1 minuto

--- src/pages/pagina-estatica.tsx
+++ src/pages/pagina-estatica.tsx
@@ -7,49 +7,60 @@
  * - A página deve ser atualizada a cada 1 minuto
  */
 
-import { useEffect, useState } from 'react';
-
+import { GetStaticProps } from 'next';
 import styles from '@/styles/lista.module.css';
 import { ICity } from '@/types/city.d';
 
-export default function Lista() {
-	const [list, setUsers] = useState<Array<ICity>>([
-		{
-			id: new Date().getTime().toString(),
-			name: 'São Paulo',
-		},
-	]);
-
-	async function getList() {
-		try {
-			const response = await fetch('/api/cities/10');
-			const data = await response.json();
-
-			if (!response.ok) throw new Error('Erro ao obter os dados');
-
-			setUsers(data);
-		} catch (error) {
-			console.error(error);
-		}
-	}
-
-	useEffect(() => {
-		getList();
-	}, []);
-
-	return (
-		<div className={styles.container}>
-			<div className={styles.content}>
-				<h2>Lista de cidades</h2>
-
-				<div data-list-container>
-					{list.map((city) => (
-						<div data-list-item key={city.id}>
-							{city.name}
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+interface ListaProps {
+  list: Array<ICity>;
 }
+
+const Lista: React.FC<ListaProps> = ({ list }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h2>Lista de cidades</h2>
+        <div data-list-container>
+          {list.map((city) => (
+            <div data-list-item key={city.id}>
+              {city.name}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Obtendo propriedades estáticas
+export const getStaticProps: GetStaticProps = async () => {
+  async function getList(): Promise<ICity[]> {
+    try {
+      const response = await fetch('http://localhost:8080/api/cities/10'); 
+      const data = await response.json();
+
+      if (!response.ok) throw new Error('Erro ao obter os dados');
+
+      return data;
+    } catch (error) {
+      console.error(error);
+      return [
+        {
+          id: new Date().getTime().toString(),
+          name: 'Erro ao obter os dados',
+        },
+      ];
+    }
+  }
+
+  const list = await getList();
+
+  return {
+    props: {
+      list,
+    },
+    revalidate: 60, // Revalidar a página a cada 1 minuto
+  };
+};
+
+export default Lista;
