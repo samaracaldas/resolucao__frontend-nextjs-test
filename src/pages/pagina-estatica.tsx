a76312ca90cf8c9174b8fe0cac2c24e08ed582a7
Samara Caldas <samaracaldass@hotmail.com>
2024-06-06 11:44:45 -0300

    Update pagina-estatica.tsx
    Comentando o código para melhor entendimento e explicação da solução.

--- src/pages/pagina-estatica.tsx
+++ src/pages/pagina-estatica.tsx
@@ -7,10 +7,19 @@
  * - A página deve ser atualizada a cada 1 minuto
  */
 
-import { GetStaticProps } from 'next';
+
+import { GetStaticProps } from 'next'; // Importanção do GetStaticProps do Next.js para gerar propriedades estáticas
 import styles from '@/styles/lista.module.css';
 import { ICity } from '@/types/city.d';
 
+/**
+ O estado das cidades (list) era gerenciado utilizando useState, e a função getList era chamada dentro de um useEffect para buscar
+ os dados das cidades quando o componente montava. Isso é adequado para páginas dinâmicas, mas para tornar a página estática, é preciso
+ remover o estado local e a lógica de efeito. */
+
+
+
+// Define a estrutura das propriedades esperadas pelo componente. 
 interface ListaProps {
   list: Array<ICity>;
 }
@@ -32,11 +41,14 @@ const Lista: React.FC<ListaProps> = ({ list }) => {
   );
 };
 
-// Obtendo propriedades estáticas
+/** A função getList, que anteriormente buscava os dados das cidades, foi movida para dentro de getStaticProps. Isso garante que os dados sejam
+ buscados apenas no momento da construção da página, não durante a renderização. A propriedade list é passada para o componente Lista como uma 
+ propriedade estática.*/
+
 export const getStaticProps: GetStaticProps = async () => {
   async function getList(): Promise<ICity[]> {
     try {
-      const response = await fetch('http://localhost:8080/api/cities/10'); 
+      const response = await fetch('http://localhost:8080/api/cities/10');  // Chamada à API para obter a lista de cidades
       const data = await response.json();
 
       if (!response.ok) throw new Error('Erro ao obter os dados');
