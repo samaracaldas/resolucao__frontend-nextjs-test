104afe453edb1b9fa2ef3f69c6da6ebc97af80ff
Samara Caldas <samaracaldass@hotmail.com>
2024-06-06 11:10:07 -0300

    7 - Ciclo de Vida
    - No evento de montagem deste component, deve ser registrados os seguintes events listeners:
       - onCounterMount
       - onCounterUnmount
       - onCounterUpdate
     - Os eventos devem ser disparados no componente Counter, seguindo o ciclo de vida do mesmo
     - Ao atualizar o contador, deverá ser passado o valor atualizado no evento onCounterUpdate, e quando o valor chegar a 10, o Counter deve ser desmontado.
    
     (Opcional)
    - Ao observar os eventos, você verá que eles são disparados mais de uma vez, isso acontece porque o componente Counter é desmontado e montado novamente, e os eventos são registrados novamente, isto é um problema comum no nextjs, você deve resolver este problema

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,32 +1,53 @@
 import { useState, useEffect } from 'react';
 
+
 type CounterProps = {
-	initialCount: number;
+  initialCount: number; 
+  // Adicão das props que serão utilizadas nos useEffects para chamar as funções de callback nos momentos corretos.
+  onCounterMount: () => void; 
+  onCounterUnmount: () => void;
+  onCounterUpdate: (count: number) => void; 
 };
 
-export const Counter: React.FC<CounterProps> = ({ initialCount }) => {
-	const [count, setCount] = useState(initialCount);
+// Declaração do componente Counter como uma função componente do React
+const Counter: React.FC<CounterProps> = ({
+  initialCount,
+  onCounterMount,
+  onCounterUnmount,
+  onCounterUpdate,
+}) => {
+  const [count, setCount] = useState(initialCount);
+  // Incremento do contador
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1); 
+  };
 
-	useEffect(() => {
-		console.log('Componente montado!');
+  // Efeito executado quando o componente é montado ou atualizado
+  useEffect(() => {
+    onCounterMount(); // Chama a função de montagem do contador
+    return () => {
+      onCounterUnmount(); // Chama a função de desmontagem do contador quando o componente é desmontado
+    };
+  }, [onCounterMount, onCounterUnmount]); 
 
-		return () => {
-			console.log('Componente desmontado!');
-		};
-	}, []);
+  // Efeito executado quando o estado do contador é alterado
+  useEffect(() => {
+    onCounterUpdate(count); // Chama a função de atualização do contador com o valor atualizado
+    if (count === 10) {
+      onCounterUnmount(); // Se o contador atingir 10, chama a função de desmontagem do contador
+    }
+  }, [count, onCounterUpdate, onCounterUnmount]); 
 
-	useEffect(() => {
-		console.log('Componente atualizado!');
-	});
 
-	const handleIncrement = () => {
-		setCount((prevCount) => prevCount + 1);
-	};
+  // Se o contador atingir 10, o componente não é renderizado
+  if (count === 10) return null;
 
-	return (
-		<div>
-			<h2>Contador: {count}</h2>
-			<button onClick={handleIncrement}>Incrementar +</button>
-		</div>
-	);
+  return (
+    <div>
+      <h2>Contador: {count}</h2>
+      <button onClick={handleIncrement}>Incrementar +</button>
+    </div>
+  );
 };
+
+export { Counter }; 
--- src/pages/ciclo-de-vida.tsx
+++ src/pages/ciclo-de-vida.tsx
@@ -17,61 +17,71 @@
 
 import { GetServerSideProps } from 'next/types';
 
-import styles from '@/styles/ciclo-de-vida.module.css';
-import { Counter } from '@/components/Counter';
-import { useEffect, useState } from 'react';
+import styles from '@/styles/ciclo-de-vida.module.css'; 
+import { Counter } from '@/components/Counter'; 
+import { useState } from 'react'; 
+
 
 type CicloDeVidaProps = {
-	initialCount: number;
+  initialCount: number; 
 };
 
+
 export default function CicloDeVida({ initialCount }: CicloDeVidaProps) {
-	const [showCounter, setShowCounter] = useState(false);
-	const [count, setCount] = useState(0);
+  const [showCounter, setShowCounter] = useState(false);
 
-	function handleOcultCounterClick() {
-		setShowCounter((prevState) => !prevState);
-	}
+  const handleOcultCounterClick = () => {
+    setShowCounter((prevState) => !prevState);
+  };
 
-	useEffect(() => {
-		window.addEventListener('onCounterMount', (event: CustomEventInit) => {
-			console.log('onCounterMount');
-		});
+  /** 
+  - Os event listeners estavam sendo adicionados utilizando window.addEventListener,o que não é o padrão para lidar com eventos personalizados no React.
+  - Os eventos foram transferidos para o componente Counter, como especificado nas instruções, portanto, o hook 'useEffect' não é mais necessário e foi
+    substituido por callbacks que reagem ao eventos do clico de vida do componente Counter e executa as ações apropriadas em resposta a esses eventos. */
+  const handleCounterMount = () => {
+    console.log('onCounterMount');
+  };
 
-		window.addEventListener('onCounterUnmount', (event: CustomEventInit) => {
-			console.log('onCounterUnmount');
-		});
+  // Função de callback chamada quando o contador é desmontado
+  const handleCounterUnmount = () => {
+    console.log('onCounterUnmount');
+  };
 
-		window.addEventListener('onCounterUpdate', (event: CustomEventInit) => {
-			console.log('onCounterUpdate');
-		});
-	}, []);
+  // Função de callback chamada quando o contador é atualizado
+  const handleCounterUpdate = (count: number) => {
+    console.log('onCounterUpdate', count);
+  };
 
-	return (
-		<div className={styles.container}>
-			<div>
-				<button type="button" onClick={handleOcultCounterClick}>
-					{showCounter ? 'Ocultar contador' : 'Mostrar contador'}
-				</button>
+  return (
+    <div className={styles.container}> 
+      <div>
+        <button type="button" onClick={handleOcultCounterClick}>
+          {showCounter ? 'Ocultar contador' : 'Mostrar contador'}
+        </button>
 
-				{showCounter && (
-					<>
-						<h1>Exemplo de Ciclo de vida</h1>
+        {showCounter && (
+          <>
+            <h1>Exemplo de Ciclo de vida</h1>
 
-						<div data-content>
-							<Counter initialCount={initialCount} />
-						</div>
-					</>
-				)}
-			</div>
-		</div>
-	);
+            <div data-content>
+              <Counter
+                initialCount={initialCount}
+                onCounterMount={handleCounterMount}
+                onCounterUnmount={handleCounterUnmount}
+                onCounterUpdate={handleCounterUpdate}
+              />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps<CicloDeVidaProps> = async () => {
-	return {
-		props: {
-			initialCount: 0,
-		},
-	};
+  return {
+    props: {
+      initialCount: 0, 
+    },
+  };
 };
